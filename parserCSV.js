9262653ed375a04f2385dd30f75dcb65a4f781bd
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-05-22 19:21:50 +0200

    Database final format

--- parserCSV.js
+++ parserCSV.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+// Program that adapts a database from space-track to a desired format
+// since some of the orbital elements are missing, they are randomly generated
 var parse = require('csv-parse');
+var generate = require('csv-generate');
 const fs = require('fs');
 const _ = require('lodash');
 
@@ -15,15 +18,31 @@ fs.readFile('./a.csv', (err, data) => {
 		let ARGP = Math.random()*360;
 		// entre 0 i 360
 		let M = Math.random()*360;
-		let randomElements = {
-			RAAN: RAAN,
-			ARGP: ARGP,
-			M: M
-		}
 		_.forEach(json, o => {
+			let randomElements = {
+				RAAN: RAAN,
+				epoch: 57400,
+				ARGP: ARGP,
+				M: M,
+				e: (parseInt(o.APOGEE)-parseInt(o.PERIGEE))/(parseInt(o.APOGEE)+parseInt(o.PERIGEE)),
+				b: (parseInt(o.APOGEE) + parseInt(o.PERIGEE))/2
+			}
 			_.assign(o, randomElements)
+
 		})
-		json = JSON.stringify(json);
-		fs.writeFile('objects_in_orbit.json', json, 'utf8');
+		json = _.map(json, o => _.omit(o,["APOGEE","PERIGEE","PERIOD","RCSVALUE","COMMENT","RCS_SIZE","COUNTRY","LAUNCH","NORAD_CAT_ID","OBJECT_ID",]));
+		// json = JSON.stringify(json);
+		// fs.writeFile('objects_in_orbit.json', json, 'utf8');
+		let csv = "";
+		_.forEach(json, o => {
+			csv = csv + o.OBJECT_NAME.split(' ').join('_') +" "+o.b+" "+o.e+" "+o.INCLINATION+" "+o.ARGP+" "+o.RAAN+" "+o.M+" "+o.epoch+"\n";
+		})
+		fs.appendFile('database.csv', csv, function (err) {
+			if (err) {
+				throw new Error('Error with writing to file');
+			}
+		})
+
 	});
 })
+

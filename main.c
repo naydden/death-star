f72a92c66533acdf23a26beceb8d2bf6b79fadf8
Boyan Naydenov <boyan.naydenov@gmail.com>
2017-05-26 18:44:40 +0200

    Started parallelzation of SGP4

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <getopt.h>
 #include <math.h>
-
+#include "mpi.h"
 #include "sgp_lib/sgdp4h.h"
 #include "lib/ReadDB.h"
 
@@ -11,10 +11,32 @@
 long int GM = 3.98600415e5;
 long int RT = 6371;
 
+void checkr(int r,char *txt) { 
+  if (r!=MPI_SUCCESS) {
+    fprintf(stderr,"Error: %s\n",txt);
+    exit(-1);
+  }
+}
+
+int quisoc() {
+  int a,b;
+  a=MPI_Comm_rank(MPI_COMM_WORLD,&b);
+  checkr(a,"quisoc");
+  return(b);
+}
+
+int quants() {
+  int a,b;
+  a=MPI_Comm_size(MPI_COMM_WORLD,&b);
+  checkr(a,"quants");
+  return(b);
+}
+
+
 //  returns x,y,z vector for a given time.
 void xyz_position(double jd, xyz_t *pos) {
 	// satpos_xyz propagates the orbital elements given in init_sgpd4 and converts to xyz
-	if(satpos_xyz(jd, pos, NULL) == SGDP4_ERROR) exit(0);
+	if(satpos_xyz(jd, pos, NULL) == SGDP4_ERROR) printf("SGP4_ERROR");
 	return;
 }
 
@@ -24,8 +46,8 @@ int is_crash(xyz_t *sat_pos, xyz_t *deb_pos, int d) {
 	double distance;
 	distance = sqrt((sat_pos->x - deb_pos->x)*(sat_pos->x - deb_pos->x) + (sat_pos->y - deb_pos->y)*(sat_pos->y - deb_pos->y) + (sat_pos->z - deb_pos->z)*(sat_pos->z - deb_pos->z));
 
-	printf("Distance: %f\n", distance);
-	printf("Security: %d\n", d);
+	// printf("Distance: %f\n", distance);
+	// printf("Security: %d\n", d);
 	if ( distance <= d) {
 		return 1;
 	}
@@ -35,26 +57,27 @@ int is_crash(xyz_t *sat_pos, xyz_t *deb_pos, int d) {
 int deb_valid(KEP *deb_object) {
 	//  checks mean motion
 	double mean_motion = (86400/(2*M_PI*sqrt((RT+deb_object->sma)*(RT+deb_object->sma)*(RT+deb_object->sma)/GM)));
-	printf("%f\n",mean_motion);
-	if( mean_motion > 18 ) {
+	double eccentricity = deb_object->ecc;
+	// printf("Mean Motion: %f\n",mean_motion);
+	if( mean_motion > 18 || eccentricity >= 0.98 ) {
 		return 1;
 	}
 	return 0;
 }
 
 void check_sgdp4(int *imode) {
-	switch(*imode)
-	{
-	case SGDP4_ERROR     :  printf("# SGDP error\n"); break;
-	case SGDP4_NOT_INIT  :  printf("# SGDP not init\n"); break;
-	case SGDP4_ZERO_ECC  :  printf("# SGDP zero ecc\n"); break;
-	case SGDP4_NEAR_SIMP :  printf("# SGP4 simple\n"); break;
-	case SGDP4_NEAR_NORM :  printf("# SGP4 normal\n"); break;
-	case SGDP4_DEEP_NORM :  printf("# SDP4 normal\n"); break;
-	case SGDP4_DEEP_RESN :  printf("# SDP4 resonant\n"); break;
-	case SGDP4_DEEP_SYNC :  printf("# SDP4 synchronous\n"); break;
-	default: 				printf("# SGDP mode not recognised!\n");
-	}
+	// switch(*imode)
+	// {
+	// case SGDP4_ERROR     :  printf("# SGDP error\n"); break;
+	// case SGDP4_NOT_INIT  :  printf("# SGDP not init\n"); break;
+	// case SGDP4_ZERO_ECC  :  printf("# SGDP zero ecc\n"); break;
+	// case SGDP4_NEAR_SIMP :  printf("# SGP4 simple\n"); break;
+	// case SGDP4_NEAR_NORM :  printf("# SGP4 normal\n"); break;
+	// case SGDP4_DEEP_NORM :  printf("# SDP4 normal\n"); break;
+	// case SGDP4_DEEP_RESN :  printf("# SDP4 resonant\n"); break;
+	// case SGDP4_DEEP_SYNC :  printf("# SDP4 synchronous\n"); break;
+	// default: 				printf("# SGDP mode not recognised!\n");
+	// }
 }
 void print_help(int exval) {
 	printf("sat-alarm [-h] -t <object-tle-file.txt> -d <characteristic-diamter-of-satellite in [m]> -f <times-the-diameter-for-security-zone> \n\n");
@@ -67,6 +90,24 @@ void print_help(int exval) {
 
 int main(int argc, char **argv)
 {
+// parallel stuff
+	int r;
+	r=MPI_Init(&argc,&argv); checkr(r,"init");
+
+	//  num of debris object to consider till 17538
+	int n = 17000;
+
+	int a1=quisoc()*n/quants()+1;
+	int a2=(quisoc()+1)*n/quants();
+
+	KEP *deb_object;
+	deb_object = read_sat(a1-1, a2);
+
+	int time_comm = 10; //s
+	int collision = 0;
+	int CollisionTime;
+	int CollisionTimeG;
+	char collider[100];
 
 //  an option for the satellite TLE file (-t)
 //  an option that gives a characteristic diamter of the satellite. A satellite will be treated as a  sphere. (-d)
@@ -132,17 +173,11 @@ int main(int argc, char **argv)
 	FILE *tle_sat;
 	tle_sat = fopen(tle,"rb");
 
-	KEP *deb_object;
-	//  num of debris object to consider till 17538
-	int n = 3;
-	deb_object = read_sat(27, 30);
-	printf("%f\n", deb_object[2].sma);
-
 	if (!tle_sat) {
 		fprintf(stderr, "Error while reading satellite TLE.'\n");
 		return 1;
 	}
-
+	int i = 0;
 	// read_twoline returns orbital elements out of a TLE
 	if (read_twoline(tle_sat, 0, &orb) == 0) {
 		print_orb(&orb);
@@ -151,7 +186,7 @@ int main(int argc, char **argv)
 		imode = init_sgdp4(&orb);
 		check_sgdp4(&imode);
 
-		for(ii=0; ii <= 4; ii++)
+		for(ii=0; ii <= 86400; ii++)
 			{
 				if(ii != iend) {
 					tsince=ts + ii*delta;
@@ -174,12 +209,13 @@ int main(int argc, char **argv)
 					tsince,
 					sat_pos.x, sat_pos.y, sat_pos.z);
 				/*********** DEBRIS **************************************/
-				for(int deb=0; deb < n; deb++) {
+				for(int deb=a1; deb <= a2; deb++) {
 					orbit_t orb_deb;
-
+					// printf("%d\n", deb);
+					// printf("%s\n", deb_object[deb].name);
 					if (deb_valid(&deb_object[deb]) == 1) continue;
 
-					orb_deb.ep_year = (int)deb_object[deb].epoch/365+100;
+					orb_deb.ep_year = (int)deb_object[deb].epoch/365+2000;
 					orb_deb.ep_day = 1.0;
 					orb_deb.rev = 86400/(2*M_PI*sqrt((RT+deb_object[deb].sma)*(RT+deb_object[deb].sma)*(RT+deb_object[deb].sma)/GM));
 					orb_deb.bstar = 1.0608e-05;
@@ -192,22 +228,43 @@ int main(int argc, char **argv)
 					orb_deb.norb = orb_deb.rev*(int)deb_object[deb].epoch;
 					orb_deb.satno = 2;
 
-					printf("---------\n");
-					print_orb(&orb_deb);
+					// printf("---------\n");
+					// print_orb(&orb_deb);
 
 					imode = init_sgdp4(&orb_deb);
 					check_sgdp4(&imode);
 					//  stops program if data is not valid for SGDP
-					if(imode == SGDP4_ERROR) exit(1);
+					if(imode == SGDP4_ERROR) continue;
 					xyz_position(jd, &deb_pos);
-					if (is_crash(&sat_pos,&deb_pos, diameter*security_ratio) == 1) {
-						printf("Crashed with %s\n", deb_object[deb].name);
-						printf("Crashed time %lf\n",tsince);
-						exit(0);
+					// if block is entered only when there have not been any other collision until now.
+					if (is_crash(&sat_pos,&deb_pos, diameter*security_ratio) == 1 && collision == 0) {
+						// printf("Crashed with %s\n", deb_object[deb].name);
+						// printf("Crashed time %lf\n",tsince);
+						collision = 1;
+						CollisionTime = tsince;
+						strcpy(collider,deb_object[deb].name);
+						break;
 					}
 
 				}
+
+				if ((int)tsince%time_comm==0) {
+					r=MPI_Barrier(MPI_COMM_WORLD);
+					MPI_Allreduce(&collision, &collision, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+					if(collision==1){
+						MPI_Allreduce(&CollisionTime, &CollisionTimeG, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+						if(CollisionTime==CollisionTimeG){
+							printf("Collider: %s Time of collision: %d \n", collider, CollisionTimeG);
+						}
+							MPI_Finalize();
+							exit(0);
+					}
+				}
 			}
 		}
+
+	if(quisoc()==0)	printf("No collision detected \n");
+
+	MPI_Finalize();
 	exit(0);
 }
